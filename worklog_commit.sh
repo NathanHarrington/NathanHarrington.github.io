11d2dfa18a50cf1f68fb3757915cb8204b5b216e
Nathan Harrington <harrington.nathan@gmail.com>
2018-02-09 10:17:14 -0500

    root cleanup

--- worklog_commit.sh
+++
@@ -1,5 +0,0 @@
-export PATH=/home/nharrington/miniconda2/bin:$PATH
-source activate pelican_env
-pelican --theme ../pelican-themes/personal-bootstrap3/ --output ./
-git commit -a -m "work log"
-git push origin master
